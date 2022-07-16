3dc2b1f251f2c415cc5a40f8b0d0cce317199e3a
Andy Pfister <andy.pfister@simplificator.com>
2022-07-16 17:40:52 +0200

    Also ask to read private playlists

--- config/initializers/spotify.rb
+++ config/initializers/spotify.rb
@@ -3,7 +3,7 @@ RSpotify::authenticate(Rails.application.credentials.spotify_client_id, Rails.ap
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, Rails.application.credentials.spotify_client_id, Rails.application.credentials.spotify_client_secret, scope: 'playlist-read-collaborative'
+  provider :spotify, Rails.application.credentials.spotify_client_id, Rails.application.credentials.spotify_client_secret, scope: 'playlist-read-private playlist-read-collaborative'
 end
 
 OmniAuth.config.allowed_request_methods = [:post, :get]
